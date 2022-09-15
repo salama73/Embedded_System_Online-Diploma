f60db6e6fe060bc56bb8148014468142be4e351c
eng salama <salama.com>
2022-09-14 20:40:37 -0700

    unit_8_lesson_5_SPI Driver to  Stm32f103c6

---
+++ Second_term/unit_8/lesson_5/SPI_Driver_stm32f103x/Debug/stm32_f103c6_Drivers/stm32_f103c6_SPI.d
@@ -0,0 +1,11 @@
+stm32_f103c6_Drivers/stm32_f103c6_SPI.o: \
+ ../stm32_f103c6_Drivers/stm32_f103c6_SPI.c \
+ C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_SPI.h \
+ C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h \
+ C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h
+
+C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_SPI.h:
+
+C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h:
+
+C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h:
