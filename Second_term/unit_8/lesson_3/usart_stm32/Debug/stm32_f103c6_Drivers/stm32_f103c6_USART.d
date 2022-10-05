2cf610ec8e96202f98ea428a5c34b2d423403ec0
Salama Mohamed <slamh7138@gmail.com>
2022-10-05 03:17:29 +0200

    learn in depth

--- Second_term/unit_8/lesson_3/usart_stm32/Debug/stm32_f103c6_Drivers/stm32_f103c6_USART.d
+++ Second_term/unit_8/lesson_3/usart_stm32/Debug/stm32_f103c6_Drivers/stm32_f103c6_USART.d
@@ -1,14 +1,14 @@
 stm32_f103c6_Drivers/stm32_f103c6_USART.o: \
  ../stm32_f103c6_Drivers/stm32_f103c6_USART.c \
- C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_USART.h \
- C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h \
- C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_RCC.h \
- C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h
+ D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_USART.h \
+ D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32f103x6.h \
+ D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_RCC.h \
+ D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h
 
-C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_USART.h:
+D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_USART.h:
 
-C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h:
+D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32f103x6.h:
 
-C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_RCC.h:
+D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_RCC.h:
 
-C:/Users/elmnshawy/STM32CubeIDE/workspace_1.4.0/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h:
+D:/Deploma\ Assignment/Deploma\ Assignment_Code/Second_term/unit_8/lesson_3/usart_stm32/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h:
