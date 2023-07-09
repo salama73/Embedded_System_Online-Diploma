0db31e8ba3e3c9be642dec5a221ef45aea9d0d3b
Salama Mohammed <slamh7138@gmail.com>
2023-07-09 16:31:03 +0300

    Canoe

--- Third_term/MRTOS/Debug/stm32_f103c6_Drivers/stm32_f103c6_GPIO.d
+++
@@ -1,8 +0,0 @@
-stm32_f103c6_Drivers/stm32_f103c6_GPIO.o: \
- ../stm32_f103c6_Drivers/stm32_f103c6_GPIO.c \
- C:/Users/ELBOSTAN/Desktop/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h \
- C:/Users/ELBOSTAN/Desktop/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h
-
-C:/Users/ELBOSTAN/Desktop/Drivers/stm32_f103c6_Drivers/inc/stm32_f103c6_GPIO.h:
-
-C:/Users/ELBOSTAN/Desktop/Drivers/stm32_f103c6_Drivers/inc/stm32f103x6.h:
